b3dcc377cadc1924d51875ddc29ef0f3262299a4
vipul neme <vipulnema2610@gmail.com>
2017-07-22 16:08:51 +0530

    String_splite_custom_function

---
+++ JS fuctions/mySplite.js
@@ -0,0 +1,15 @@
+String.prototype.mySplite = function mySplite (seperator) {
+	var arr = [];
+	var length = this.String;
+	str = "";
+	for(var i =0; i<this.length; i++){
+		if(this[i]==seperator){
+			arr.push(str);
+			str = "";
+		}else {
+			str += this[i];
+		}
+	}
+	arr.push(str);
+	return arr;
+}
